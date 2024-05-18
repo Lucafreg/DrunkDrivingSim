78b93390a793679cd08f9ce6bb15a74d87359820
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-05-18 22:49:18 +0100

    road prefab

---
+++ Game Jam Driving Sim/Assets/Dev/Matt/RoadGeneration.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generates the obstacles along the road 
+/// </summary>
+public class RoadGeneration : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
