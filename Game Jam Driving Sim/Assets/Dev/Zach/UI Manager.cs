74aa94596cadbf87b83729202ca64f171790da14
Trem13roz <6tremz14@solent.ac.uk>
2024-05-19 19:10:41 +0100

    Added loss screen and main menu scene with scene manager and build settings

--- Game Jam Driving Sim/Assets/Dev/Zach/UI Manager.cs
+++ Game Jam Driving Sim/Assets/Dev/Zach/UI Manager.cs
@@ -4,16 +4,19 @@ using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
 
     public GameObject UIMAnager;
-    public static float CurrentScene;
+    public int MainMenuScene,GameScene;
     public GameObject PlayerHealthUI;
     public Slider PlayerHealthSlider;
     public GameObject MainMenuUI;
     public GameObject PauseMenuUI, CreditsMenuUI;
+    
 
     public bool Resumed;
     private void Awake()
@@ -27,6 +30,9 @@ public class UIManager : MonoBehaviour
         UIMAnager = GameObject.FindGameObjectWithTag("UIManager");
         PlayerHealthUI = GameObject.FindGameObjectWithTag("PlayerHealthUI");
         //PlayerHealthSlider = PlayerHealthUI.GetComponent<Slider>();
+        Resumed = true;
+        MainMenuScene = 0;
+        GameScene = 1;
     }
 
     // Update is called once per frame
@@ -48,7 +54,8 @@ public class UIManager : MonoBehaviour
     }
     public void OnPlay()
     {
-        MainMenuUI.SetActive(false);
+        
+        SceneManager.LoadScene(GameScene);
     }
     public void OnPause()
     {
@@ -65,11 +72,10 @@ public class UIManager : MonoBehaviour
         CreditsMenuUI.SetActive(true);
         MainMenuUI.SetActive(false);
     }
-    public void OnMainMenuPause()
+    public void OnMainMenu()
     {
-        MainMenuUI.SetActive(true);
-        PauseMenuUI.SetActive(false);
-        Time.timeScale = .0f;
+        
+        SceneManager.LoadScene(MainMenuScene);
     }
     public void OnMainMenuCredits()
     {
